c4b84a143c49b0f735e09d11ac899353fdc37adc
karolsobon <karolsobon@aol.com>
2024-08-04 17:14:11 +0200

    Ci fix

--- cypress.config.js
+++ cypress.config.js
@@ -3,24 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Integrate cypress-mochawesome-reporter plugin
       require('cypress-mochawesome-reporter/plugin')(on);
-      
-      // You can add other plugins here as needed
     },
-    baseUrl: 'https://moodfilm.netlify.app/', // Set the base URL for your app
-    supportFile: 'cypress/support/e2e.js', // Ensure support file is set
-    reporter: 'cypress-mochawesome-reporter', // Use mochawesome reporter
+    reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
-      reportDir: 'cypress/reports', // Directory where reports are stored
-      overwrite: true, // Prevent overwriting of previous reports
-      html: true, // Generate HTML reports
-      json: false, // Generate JSON reports
-      reportPageTitle: 'MoodFilm Test Report', // Set a custom report page title
-      embeddedScreenshots: true, // Embed screenshots in the report
-      inlineAssets: true, // Include CSS and JS inline for self-contained report
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
     },
-    video: false, // Capture video of test runs
-    screenshotOnRunFailure: true, // Capture screenshots on test failure
   },
 });
