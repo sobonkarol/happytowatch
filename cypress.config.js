c885b5007afb026b3af9ee3eeb3dc488eaa0e2db
karolsobon <karolsobon@aol.com>
2024-08-04 17:42:34 +0200

    reports fix

--- cypress.config.js
+++ cypress.config.js
@@ -4,13 +4,14 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
       reportDir: 'cypress/reports',
-      overwrite: false,
-      html: false,
-      json: true
+      overwrite: true, // Allow overwriting reports for each run
+      html: true,      // Enable HTML report generation
+      json: true       // Enable JSON report generation
     },
   },
 });
